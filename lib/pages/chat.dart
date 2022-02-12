2e992f54dc8c561b88a58112ee370564f5a9ba58
Cameron Greene <cam.greene@gmail.com>
2022-02-12 13:05:57 -0800

    Finished Structure for Chat Page
    Class, layout, colors, and text for chat page.

--- lib/pages/chat.dart
+++ lib/pages/chat.dart
@@ -2,6 +2,8 @@
 
 import 'package:flutter/material.dart';
 
+import '../classes/chatMessagesModel.dart';
+
 class ChatPage extends StatefulWidget {
   const ChatPage({Key? key}) : super(key: key);
 
@@ -15,6 +17,36 @@ class _ChatPageState extends State<ChatPage> {
     return Container(
       child: Stack(
         children: <Widget>[
+          ListView.builder(
+            itemCount: messages.length,
+            shrinkWrap: true,
+            padding: EdgeInsets.only(top: 10, bottom: 10),
+            physics: NeverScrollableScrollPhysics(),
+            itemBuilder: (context, index) {
+              return Container(
+                padding:
+                    EdgeInsets.only(left: 14, right: 14, top: 10, bottom: 10),
+                child: Align(
+                  alignment: (messages[index].messageType == "receiver"
+                      ? Alignment.topLeft
+                      : Alignment.topRight),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: (messages[index].messageType == "receiver"
+                          ? Colors.grey.shade200
+                          : Colors.blue[200]),
+                    ),
+                    padding: EdgeInsets.all(16),
+                    child: Text(
+                      messages[index].messageContent,
+                      style: TextStyle(fontSize: 15),
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
           Align(
             alignment: Alignment.bottomLeft,
             child: Container(
@@ -64,3 +96,14 @@ class _ChatPageState extends State<ChatPage> {
     );
   }
 }
+
+List<ChatMessage> messages = [
+  ChatMessage(messageContent: "Hello, Will", messageType: "receiver"),
+  ChatMessage(messageContent: "How have you been?", messageType: "receiver"),
+  ChatMessage(
+      messageContent: "Hey Kriss, I am doing fine dude. wbu?",
+      messageType: "sender"),
+  ChatMessage(messageContent: "ehhhh, doing OK.", messageType: "receiver"),
+  ChatMessage(
+      messageContent: "Is there any thing wrong?", messageType: "sender"),
+];
