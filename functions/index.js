38c16f77c5dfff3925753e70223eb440e09fa4b4
cyin100 <46613498+cyin100@users.noreply.github.com>
2022-02-12 13:36:08 -0800

    Attemped retrieve info from claim

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,37 @@
 const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.processClaim = functions.firestore
+    .document('claim/{id}')
+    .onCreate((snap, context) => {
+        const newValue = snap.data();
+        const jobId = newValue.jobId;
+
+        //on getting data https://firebase.google.com/docs/firestore/query-data/get-data
+
+        const jobsRef = db.collection('job').doc(jobId);
+        const doc = await jobsRef.get();
+
+        if (!doc.exists) {
+            console.log('No such document!');
+        } else {
+            console.log('Document data:', doc.data());
+        }
+    });
+
+    /*
+    const message = {
+            data: {
+                title: 'Claim accepted',
+                body: 'Someone accepted your job request!'
+            },
+            token: registrationToken
+        };
+
+        getMessaging().send(message)
+        .then((response) => {
+            console.log('Successfully sent message:', response);
+        })
+        .catch((error) => {
+            console.log('Error sending message:', error);
+        }); 
+*/
