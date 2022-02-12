d2be97cfc71d8c4d0b1cd2456e6570ed51efdab0
Jordan Mosakowski <35510468+jordanmosakowski@users.noreply.github.com>
2022-02-12 10:42:58 -0800

    added pages

---
+++ lib/pages/map.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MapPage extends StatefulWidget {
+  const MapPage({ Key? key }) : super(key: key);
+
+  @override
+  _MapPageState createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
---
+++ lib/pages/request.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class RequestPage extends StatefulWidget {
+  const RequestPage({ Key? key }) : super(key: key);
+
+  @override
+  _RequestPageState createState() => _RequestPageState();
+}
+
+class _RequestPageState extends State<RequestPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
