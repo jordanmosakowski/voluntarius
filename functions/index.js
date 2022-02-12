575b59ff421307d73ef53cd6cc85c94093edeeef
cyin100 <46613498+cyin100@users.noreply.github.com>
2022-02-12 13:49:48 -0800

    Push notification functionality

--- functions/index.js
+++ functions/index.js
@@ -2,36 +2,40 @@ const functions = require("firebase-functions");
 
 exports.processClaim = functions.firestore
     .document('claim/{id}')
-    .onCreate((snap, context) => {
+    .onCreate(async(snap, context) => {
         const newValue = snap.data();
         const jobId = newValue.jobId;
-
-        //on getting data https://firebase.google.com/docs/firestore/query-data/get-data
-
         const jobsRef = db.collection('job').doc(jobId);
-        const doc = await jobsRef.get();
+        const job = await jobsRef.get();
 
-        if (!doc.exists) {
-            console.log('No such document!');
+        if (!job.exists) {
+            console.log('Job not found');
         } else {
-            console.log('Document data:', doc.data());
+            const requestorId = job.data().requestorId;
+            const userRef = db.collection('user').doc(requestorId);
+            const user = await userRef.get();
+
+            if(!user.exists){
+                console.log('User not found');
+            }
+
+            tokens = user.data().notificationTokens;
+
+            for(i=0; i<tokens.length(); i++)
+                const message = {
+                    data: {
+                        title: 'Claim accepted',
+                        body: 'Someone accepted your job request!'
+                    },
+                    token: tokens[i]
+                };
+        
+                getMessaging().send(message)
+                .then((response) => {
+                    console.log('Successfully sent message:', response);
+                })
+                .catch((error) => {
+                    console.log('Error sending message:', error);
+                }); 
         }
     });
-
-    /*
-    const message = {
-            data: {
-                title: 'Claim accepted',
-                body: 'Someone accepted your job request!'
-            },
-            token: registrationToken
-        };
-
-        getMessaging().send(message)
-        .then((response) => {
-            console.log('Successfully sent message:', response);
-        })
-        .catch((error) => {
-            console.log('Error sending message:', error);
-        }); 
-*/
