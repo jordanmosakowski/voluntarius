a39786ca324218d0c3e048d4dab883d2c7c0469b
cyin100 <46613498+cyin100@users.noreply.github.com>
2022-02-12 23:44:42 -0800

    Process rating

--- lib/classes/rating.dart
+++ lib/classes/rating.dart
@@ -1,36 +1,33 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Rating {
-  String reviewerId;
-  String revieweeId;
+  String raterId;
+  String ratedId;
   double rating;
   String text;
   String id;
 
   Rating(
-      {
-      required this.id,  
-      required this.reviewerId,
-      required this.revieweeId,
+      {required this.id,
+      required this.raterId,
+      required this.ratedId,
       required this.rating,
-      required this.text
-      });
-  
-  static Rating fromFirestore(DocumentSnapshot snap){
-    Map<String,dynamic> data = snap.data() as Map<String, dynamic>;
+      required this.text});
+
+  static Rating fromFirestore(DocumentSnapshot snap) {
+    Map<String, dynamic> data = snap.data() as Map<String, dynamic>;
     return Rating(
-      id: snap.id,
-      reviewerId: data['reviewerId'],
-      revieweeId: data['revieweeId'],
-      rating: data['rating'],
-      text: data['text']
-    );
+        id: snap.id,
+        raterId: data['raterId'],
+        ratedId: data['ratedId'],
+        rating: data['rating'],
+        text: data['text']);
   }
 
-  Map<String,dynamic> toJson(){
+  Map<String, dynamic> toJson() {
     return {
-      'reviewerId': reviewerId,
-      'revieweeId': revieweeId,
+      'reviewerId': raterId,
+      'revieweeId': ratedId,
       'rating': rating,
       'text': text
     };
